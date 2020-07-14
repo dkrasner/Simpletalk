f6d537889a6db6150cf5477915b691cff3624203
dkrasner <krasnerdaniel@gmail.com>
2020-07-14 18:41:37 +0200

    initial cleanup of keyword part of the ST grammar

--- test/grammar-test.js
+++ test/grammar-test.js
@@ -21,8 +21,54 @@ describe("SimpleTalk Grammar", function () {
             assert.isTrue(match.succeeded());
         });
         it ("Matching basic keywords", function () {
-            let match = g.match('right', 'keyword');
+        });
+        it ("Matching basic keywords (menu*)", function () {
+            let match = g.match('menus', 'keyword');
             assert.isTrue(match.succeeded());
+            match = g.match('menu', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('menubar', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('menuitem', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('menuitems', 'keyword');
+            assert.isTrue(match.succeeded());
+        });
+        it ("Matching basic keywords (char*)", function () {
+            let match = g.match('characters', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('character', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('char', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('chars', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('chartonum', 'keyword');
+            assert.isTrue(match.succeeded());
+        });
+        it ("Matching basic keywords (modifier keys)", function () {
+            let match = g.match('characters', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('commandkey', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('cmdkey', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('optionkey', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('controlkey', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('shiftkey', 'keyword');
+            assert.isTrue(match.succeeded());
+        });
+        it ("Matching basic keywords (item*)", function () {
+            let match = g.match('item', 'keyword');
+            assert.isTrue(match.succeeded());
+            match = g.match('items', 'keyword');
+            assert.isTrue(match.succeeded());
+        });
+        it ("Not Matching basic keywords", function () {
+            let match = g.match('menuBAD', 'keyword');
+            assert.isTrue(match.failed());
         });
         it ("Not Matching basic keywords", function () {
             let match = g.match(' beep', 'keyword');
