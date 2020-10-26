6e7762a082f104303a0cd61b3ff1a18dccc99793
Matt DeLand <matthew.deland@gmail.com>
2020-10-26 16:33:12 -0700

    use a third party library (tabulator) to power devtools table interaction (#60)
    * use a third party library (tabulator) to power devtools table interaction
    
    * ensure existing filters apply when new rows are added
    
    * let table take up 100% of it's area

--- developer/devtools/panels/panel.js
+++ developer/devtools/panels/panel.js
@@ -7,8 +7,50 @@
  * I also handle how the information is displayed.
  */
 
+var clearButton = document.getElementById("clear-button");
+var tablulatordata = [];
 
-var messageTableBody = document.getElementById("message-tbody");
+var idClick = function(e, cell) {
+    console.log("cell clicked");
+    var celltext = cell.getValue()
+    var id = celltext.match(/\(id=(.*)\)/)[1]
+    if (id) {
+        var inspectCommand = `inspect(document.querySelector('[part-id="${id}"]'))`;
+        browser.devtools.inspectedWindow.eval(inspectCommand).then(handleResult);
+    }
+}
+
+var table = new Tabulator("#tabulator-table", {
+    // height:600, having a set height of table enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+    // if performance becomes a problem, go back to this solution
+    maxHeight:"100%",
+    data:tablulatordata, //assign data to table
+    reactiveData:true,
+    layout:"fitColumns", //fit columns to width of table (optional)
+    columns:[
+        {title:"Time", field:"time", width:150},
+        {title:"Message", field:"message", headerFilter:true, formatter:"textarea"},
+        {title:"Sender", field:"sender", headerFilter:true, formatter:"textarea", cellClick: idClick},
+        {title:"Receiver", field:"receiver", headerFilter:true, formatter:"textarea", cellClick: idClick} ,
+    ],
+    // by default, this library doesn't apply existing filters to new rows, so here we manually reset
+    // those column filters when a new row shows up
+    rowAdded:function(row){
+        filters = table.getHeaderFilters();
+        table.clearFilter(true);
+        filters.forEach(function (filter) {
+            table.setHeaderFilterValue(filter.field, filter.value);
+        });
+    },
+});
+
+clearButton.onclick = function() {
+    console.log("clearing data")
+    while (tablulatordata.length > 0) {
+        tablulatordata.shift()
+    }
+}
+
 
 function handleMessageFromBackground(msg) {
     console.log("getting message from background");
@@ -16,15 +58,14 @@ function handleMessageFromBackground(msg) {
         console.log("message is not length 3!: " + msg);
         return;
     }
-    let tr = document.createElement("tr");
-    let td;
-    let messageEl = _prepMessage(msg[0]);
-    let senderEl = _prepObject(msg[1][0], msg[1][1]);
-    let receiverEl = _prepObject(msg[2][0], msg[2][1]);
-    tr.appendChild(messageEl);
-    tr.appendChild(senderEl);
-    tr.appendChild(receiverEl);
-    messageTableBody.appendChild(tr);
+    let now = new Date();
+    var j = {
+        time: now.toLocaleTimeString([], {hour12: false}) + `.${now.getMilliseconds()}`,
+        message: JSON.stringify(msg[0], null, '\t'),
+        sender: `${msg[1][0]} (id=${msg[1][1]})`,
+        receiver: `${msg[2][0]} (id=${msg[2][1]})`,
+    }
+    tablulatordata.push(j)
 }
 
 function _prepMessage(msg){
@@ -32,19 +73,6 @@ function _prepMessage(msg){
     let pre = document.createElement("pre");
     pre.textContent = JSON.stringify(msg, null, '\t');
     td.appendChild(pre);
-    return td;
-}
-
-function _prepObject(name, id){
-    let td = document.createElement("td");
-    let objectStr = `${name} (id=${id})`;
-    // add the ability to click on any object and see it's DOM view
-    // in the inspector
-    td.addEventListener("click", () => {
-        let inspectCommand = `inspect(document.getElementById(${id}))`;
-        browser.devtools.inspectedWindow.eval(inspectCommand).then(handleResult);
-    });
-    td.textContent = objectStr;
     return td;
 }
 
