6fc3629e22792a2c940334bb04b2c4a437798f3b
Eric Gade <eric.gade@gmail.com>
2020-12-04 15:42:20 -0500

    Initial commit of comment grammar rewrite
    -- Description
    Comments are still not implemented properly. This initial commit
    reimplements the basic comment lexical rule and adds two new rules
    called CommentLine (a single line comment) and CommentLines (several
    CommentLine in a row).
    
    We also expect a Statement to optionally end in a comment.
    
    I am also pushing tests for these.

---
+++ js/ohm/tests/test-comment-grammar.js
@@ -0,0 +1,69 @@
+/**
+ * Test Comments in the grammar
+ */
+
+ohm = require('ohm-js');
+// Instantiate the grammar.
+var fs = require('fs');
+var g = ohm.grammar(fs.readFileSync('./js/ohm/simpletalk.ohm'));
+
+var chai = require('chai');
+var assert = chai.assert;
+
+const matchTest = (str) => {
+    const match = g.match(str);
+    assert.isTrue(match.succeeded());
+}
+const semanticMatchTest = (str, semanticType) => {
+    const typeMatch = g.match(str, semanticType);
+    assert.isTrue(typeMatch.succeeded());
+}
+const matchAndsemanticMatchTest = (str, semanticType) => {
+    matchTest(str);
+    const typeMatch = g.match(str, semanticType);
+    assert.isTrue(typeMatch.succeeded());
+}
+const semanticMatchFailTest = (str, semanticType) => {
+    const typeMatch = g.match(str, semanticType);
+    assert.isFalse(typeMatch.succeeded());
+}
+String.prototype.unCapitalize = function() {
+    return this.charAt(0).toLowerCase() + this.slice(1);
+}
+
+describe("Comment Grammar Tests", () => {
+    describe("Basic", () => {
+        it("Can parse a basic comment", () => {
+            let str = "-- this is a comment";
+            semanticMatchTest(str, "comment");
+        });
+        it("Can parse a comment with special characters", () => {
+            let str = "-- !@#$%^&%& -- -^^@%#";
+            semanticMatchTest(str, "comment");
+        });
+        it("Can parse an empty comment", () => {
+            let str = "--";
+            semanticMatchTest(str, "comment");
+        });
+    });
+    describe("Full line comments", () => {
+        it("Can parse a single line comment", () => {
+            let str = "-- this is a comment that takes up a whole line\n";
+            semanticMatchTest(str, "CommentLine");
+        });
+        it("Can match a block of comment lines", () => {
+            let str = [
+                '-- this is the first line of the comment block',
+                '-- this is the second line of the comment block',
+                '--this is the last line of the -- comment block\n'
+            ].join("\n");
+            semanticMatchTest(str, "CommentLines");
+        });
+    });
+    describe("Inline comments", () => {
+        it("Can handle a comment that comes after a command (parses Statement)", () => {
+            let str = "put 5 into myVar -- the variable we will use";
+            semanticMatchTest(str, "Statement");
+        });
+    });
+});
