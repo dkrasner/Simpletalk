3fb2d0a7f2686fae3081c4cf150074cac6305986
Eric Gade <eric.gade@gmail.com>
2021-01-05 13:06:37 -0500

    Initial add of Repeat rules to grammar, with tests

---
+++ js/ohm/tests/test-repeat-grammar.js
@@ -0,0 +1,95 @@
+/**
+ * Repeat Grammar Tests
+ * --------------------------------
+ * Tests the grammar of looping constructs
+ */
+ohm = require('ohm-js');
+// Instantiate the grammar.
+var fs = require('fs');
+var g = ohm.grammar(fs.readFileSync('./js/ohm/simpletalk.ohm'));
+
+var chai = require('chai');
+var assert = chai.assert;
+
+const matchTest = (str) => {
+    const match = g.match(str);
+    assert.isTrue(match.succeeded());
+};
+const semanticMatchTest = (str, semanticType) => {
+    const typeMatch = g.match(str, semanticType);
+    assert.isTrue(typeMatch.succeeded());
+};
+
+const semanticMatchFailTest = (str, semanticType) => {
+    const typeMatch = g.match(str, semanticType);
+    assert.isFalse(typeMatch.succeeded());
+};
+
+describe("RepeatControlForm tests", () => {
+    describe("forNumTimes", () => {
+        it("Can do basic repeat of literal integer", () => {
+            let str = "repeat for 5 times";
+            semanticMatchTest(str, "RepeatControlForm_forNumTimes");
+        });
+        it("Can do basic repeat of variable (assume integer)", () => {
+            let str = "repeat for myCustomNum times";
+            semanticMatchTest(str, "RepeatControlForm_forNumTimes");
+        });
+        it("Fails when attempting to do a float literal", () => {
+            let str = "repeat for 0.5 times";
+            semanticMatchFailTest(str, "RepeatControlForm_forNumTimes");
+        });
+        it("Fails when attemting to use a string literal", () => {
+            let str = `repeat for "3" times`;
+            semanticMatchFailTest(str, "RepeatControlForm_forNumTimes");
+        });
+    });
+    describe("untilCondition", () => {
+        it("Can do a boolean literal", () => {
+            let str = `repeat until true`;
+            semanticMatchTest(str, "RepeatControlForm_untilCondition");
+        });
+        it("Can do a variable name as the condition", () => {
+            let str = `repeat until myVariable`;
+            semanticMatchTest(str, "RepeatControlForm_untilCondition");
+        });
+        it("Can use a conditional statement of literals", () => {
+            let str = `repeat until 2 > 3`;
+            semanticMatchTest(str, "RepeatControlForm_untilCondition");
+        });
+        it("Can use a conditional that has variable name(s)", () => {
+            let str = `repeat until myVariable is 5`;
+            semanticMatchTest(str, "RepeatControlForm_untilCondition");
+        });
+    });
+    describe("whileCondition", () => {
+       it("Can do a boolean literal", () => {
+            let str = `repeat while true`;
+            semanticMatchTest(str, "RepeatControlForm_whileCondition");
+        });
+        it("Can do a variable name as the condition", () => {
+            let str = `repeat while myVariable`;
+            semanticMatchTest(str, "RepeatControlForm_whileCondition");
+        });
+        it("Can use a conditional statement of literals", () => {
+            let str = `repeat while 2 > 3`;
+            semanticMatchTest(str, "RepeatControlForm_whileCondition");
+        });
+        it("Can use a conditional that has variable name(s)", () => {
+            let str = `repeat while myVariable is 5`;
+            semanticMatchTest(str, "RepeatControlForm_whileCondition");
+        }); 
+    });
+    describe("withStartFinish", () => {
+        it("Can use a basic variable name with literal integer range", () => {
+            let str = `repeat with myNum = 0 to 100`;
+            semanticMatchTest(str, "RepeatControlForm_withStartFinish");
+        });
+    });
+    describe("forever", () => {
+        it("Can understand the forever repeat", () => {
+            let str = `repeat `;
+            semanticMatchTest(str, "RepeatControlForm_forever");
+        });
+    });
+});
