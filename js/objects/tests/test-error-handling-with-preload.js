d5e21aa4ec78d9468193a0ea4967f394780144ed
dkrasner <krasnerdaniel@gmail.com>
2021-01-28 18:30:13 +0100

    Initial Implementation of Error Handler
    GrammarMatchError handling and markup in the scripting editor
    other errors are simply sent up to system at the moment, as before
    tests for GrammarMatchError handling

---
+++ js/objects/tests/test-error-handling-with-preload.js
@@ -0,0 +1,55 @@
+/**
+ * Tests for Error Handling 
+ * -------------------------------------------
+ */
+import chai from 'chai';
+const assert = chai.assert;
+const expect = chai.expect;
+
+let currentCard;
+let button;
+describe('Error Handling', () => {
+    describe('Setup', () => {
+        it('Can add a new button to the current card', () => {
+            let makeButtonFunction = function(){
+                currentCard = System.getCurrentCardModel();
+                let msg = {
+                    type: 'command',
+                    commandName: 'newModel',
+                    args: ['button', currentCard.id]
+                };
+                currentCard.sendMessage(msg, currentCard);
+            };
+            expect(makeButtonFunction).to.not.throw();
+        });
+        it('Can find the button object, which has NO command handlers yet', () => {
+            button = System.getCurrentCardModel().subparts.find(subpart => {
+                return subpart.type == 'button';
+            });
+            assert.exists(button);
+            assert.isEmpty(button._commandHandlers);
+        });
+    });
+    describe('Compiling a bad script throws a corresponding "GrammarMatchError"', () => {
+        it('GrammarMatchError automatically opens the script editor (if not present)', () => {
+            let firstScript = [
+                'on doSomethingFirst',
+                'not a command',
+                'end doSomethingFirst',
+            ].join('\n');
+            button.partProperties.setPropertyNamed(button, "script", firstScript);
+            let scriptEditor = window.System.findScriptEditorByTargetId(button.id);
+            assert.isNotNull(scriptEditor);
+        });
+        it('Script editor has the propertly error marked content', () => {
+            let markedUpScript = [
+                'on doSomethingFirst',
+                'not a command <<<[Expected:"end"; ruleName: "StatementList"]',
+                'end doSomethingFirst',
+            ].join('\n');
+            let scriptEditor = window.System.findScriptEditorByTargetId(button.id);
+            let textContent = scriptEditor.model.partProperties.getPropertyNamed(scriptEditor, "textContent");
+            assert.equal(markedUpScript, textContent);
+        });
+    });
+});
---
+++ js/objects/utils/errorHandler.js
@@ -0,0 +1,62 @@
+/**
+ * Error Handler
+ * ------------------------------------
+ * I am responsible for handler all
+ * System-wide errors
+ */
+
+const errorHandler = {
+
+    handle: function(aMessage){
+        switch(aMessage.name){
+            case 'GrammarMatchError':
+                return this.handleGrammarMatchError(aMessage);
+            default:
+                // if I don't know what to do with this message
+                // I send it along to the System
+                return window.System.sendMessage(aMessage);
+        }
+    },
+
+    handleGrammarMatchError: function(aMessage){
+        // first locate the script editor in question
+        let scriptEditor = window.System.findScriptEditorByTargetId(aMessage.partId);
+        if(!scriptEditor){
+            this._openScriptEditor(aMessage.partId);
+            scriptEditor = window.System.findScriptEditorByTargetId(aMessage.partId);
+        }
+        // TODO is there a more structured way to get this out of ohm?
+        let regex = /Line (?<line>\d), col (?<column>\d)/;
+        let match = aMessage.parsedScript.message.match(regex);
+        let errorLineNum = parseInt(match.groups["line"]) - 1; // ohm lines start with 1 
+        // see if the grammar rule has been identified
+        let ruleName;
+        let rightMostFailures = aMessage.parsedScript.getRightmostFailures();
+        if(rightMostFailures[1]){
+            ruleName = rightMostFailures[1].pexpr.ruleName;
+        }
+        // get some more info about what the parser expected
+        let expectedText = aMessage.parsedScript.getExpectedText();
+        let textContent = scriptEditor.model.partProperties.getPropertyNamed(scriptEditor, "textContent");
+        let textLines = textContent.split("\n");
+        // replace said text line with an error marker
+        textLines[errorLineNum] += ` <<<[Expected:${expectedText}; ruleName: "${ruleName}"]`;
+        textContent = textLines.join("\n");
+        scriptEditor.setTextValue(textContent);
+    },
+
+    _openScriptEditor: function(partId){
+        let targetView = window.System.findViewById(partId);
+        let msg = {
+            type: "command",
+            "commandName": "openScriptEditor",
+            args: [partId]
+        };
+        targetView.model.sendMessage(msg, targetView.model);
+    }
+};
+
+export {
+    errorHandler,
+    errorHandler as default
+};
