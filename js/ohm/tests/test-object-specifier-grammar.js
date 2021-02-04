4c8b0b9a411962f13f07b6ce182e36f970ed1815
Eric Gade <eric.gade@gmail.com>
2021-02-04 18:06:32 -0500

    [WIP] Saving grammar implementation progress

--- js/ohm/tests/test-object-specifier-grammar.js
+++ js/ohm/tests/test-object-specifier-grammar.js
@@ -33,6 +33,19 @@ const partTypes = [
     "svg"
 ];
 
+const numbers = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+];
+
 describe("Terminal Specifier Tests", () => {
     it("thisSystemObject", () => {
         partTypes.forEach(partName => {
@@ -59,7 +72,59 @@ describe("Terminal Specifier Tests", () => {
 describe("Partial Specifier Tests", () => {
     describe("Part by id tests", () => {
         it("Can parse a part by an integer literal id", () => {
-            
+            let str = `part id 24`;
+            semanticMatchTest(str, 'PartialSpecifier_partById');
+        });
+        it("Cannot parse a part by a float literal id", () => {
+            let str = `part id 2.3`;
+            semanticMatchFailTest(str, 'PartialSpecifier_partById');
+        });
+    });
+    describe("Part by name tests", () => {
+        it("Can parse available system part refs by name", () => {
+            partTypes.forEach(partName => {
+                let str = `${partName} "some name"`;
+                semanticMatchTest(str, 'PartialSpecifier_partByName');
+            });
+        });
+        it("Can parse a literal part by name", () => {
+            let str = `part "some part name here"`;
+            semanticMatchTest(str, 'PartialSpecifier_partByName');
+        });
+    });
+    describe("Part by index tests", () => {
+        it("Can parse system parts by integer index", () => {
+            partTypes.forEach(partName => {
+                let str = `${partName} 22`;
+                semanticMatchTest(str, 'PartialSpecifier_partByIndex');
+            });
+        });
+        it("Can parse literal part by integer index", () => {
+            let str = `part 23`;
+            semanticMatchTest(str, 'PartialSpecifier_partByIndex');
+        });
+    });
+    describe("Part by english numerical tests", () => {
+        numbers.forEach(numericName => {
+            it(`Can parse '${numericName}' system parts`, () => {
+                partTypes.forEach(partName => {
+                    let str = `${numericName} ${partName}`;
+                    semanticMatchTest(str, 'PartialSpecifier_partByNumericalIndex');
+                });
+            });
+            it(`Can parse '${numericName}' part literal`, () => {
+                let str = `${numericName} part`;
+                semanticMatchTest(str, 'PartialSpecifier_partByNumericalIndex');
+            });
+        });
+    });
+});
+
+describe("Compound Specifier Tests (non-terminal)", () => {
+    describe('partByName of:', () => {
+        it('partByName', () => {
+            let str = `button "hello" of card "some card"`;
+            semanticMatchTest(str, 'PartialSpecifier_compoundSpecifierWithoutTerminal');
         });
     });
 });
