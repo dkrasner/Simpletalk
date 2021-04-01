67b5bff00e6be7636cb19977e3ce8f9446ede5c4
Eric Gade <ecgade@pop-os.localdomain>
2021-04-01 12:47:48 -0400

    Fixing post-rebase error

--- js/objects/System.js
+++ js/objects/System.js
@@ -32,7 +32,6 @@ import AudioView from './views/AudioView.js';
 import Halo from './views/Halo.js';
 import EditorView from './views/editors/EditorView.js';
 import STNavigator from './views/navigator/Navigator.js';
-import ButtonEditorView from './views/editors/ButtonEditorView.js';
 
 import ohm from 'ohm-js';
 import interpreterSemantics from '../ohm/interpreter-semantics.js';
@@ -1542,7 +1541,7 @@ document.addEventListener('DOMContentLoaded', () => {
     window.customElements.define('st-halo', Halo);
     window.customElements.define('st-editor', EditorView);
     window.customElements.define('st-navigator', STNavigator);
-    window.customElements.define('st-button-editor', ButtonEditorView);
+    
 
     // If there is already a Navigator element in the body,
     // remove it and add a new one
