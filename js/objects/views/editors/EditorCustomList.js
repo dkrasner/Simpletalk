4f52253b49b4e98e74332fbb4f66abb9630ca1d1
Eric Gade <eric.gade@gmail.com>
2021-05-25 13:48:40 -0400

    [WIP] Property creation form working in custom pane
    Still need to implement custom property list items in this pane, but
    the creation and setting of initial values is working

--- js/objects/views/editors/EditorCustomList.js
+++ js/objects/views/editors/EditorCustomList.js
@@ -2,6 +2,20 @@
 
 // PREAMBLE
 
+const caretDownIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-caret-down" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M18 15l-6 -6l-6 6h12" transform="rotate(180 12 12)" />
+</svg>
+`;
+
+const caretRightIcon = `
+<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-caret-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M18 15l-6 -6l-6 6h12" transform="rotate(90 12 12)" />
+</svg>
+`;
+
 const templateString = `
 <style>
     :host {
@@ -19,8 +33,12 @@ const templateString = `
         width: 100%;
     }
 
+    #new-prop-area.open > #new-prop-form {
+        display:flex;
+    }
+
     #new-prop-form {
-        display: flex;
+        display: none;
         flex-direction: column;
         width: 100%;
     }
@@ -30,12 +48,64 @@ const templateString = `
         flex-direction: row;
         width: 100%;
         align-items: center;
+        margin-bottom: 1em;
     }
+
+    h3 {
+        padding: 0px;
+        margin-bottom: 1em;
+    }
+
+    #new-prop-form > .row > * {
+        margin-right: 16px;
+        padding-left: 8px;
+        padding-right: 8px;
+    }
+    #new-prop-form input {
+        outline: none;
+        border: 1px solid transparent;
+        border-bottom: 1px solid rgba(100, 100, 100, 0.7);
+        font-family: monospace;
+        padding: 6px;
+    }
+    select {
+        font-size: 1em;
+    }
+    #add-prop-dropdown-control {
+        align-items: center;
+        user-select: none;
+    }
+
+    #add-prop-dropdown-control:hover {
+        cursor: pointer;
+    }
+
+    #caret-button {
+        position: relative;
+        width: 1.5em;
+        height: 1.5em;
+        transform: rotate(0deg);
+        transition: transform 0.1s linear;
+    }
+
+    #caret-button > svg {
+        width: 100%;
+        height: 100%;
+    }
+    #new-prop-area.open #caret-button {
+        transform: rotate(90deg);
+        transition: transform 0.1s linear;
+    }
+
 </style>
 <div id="new-prop-area">
-    <h4>Add New Property</h4>
+    <div class="row" id="add-prop-dropdown-control">
+        <h3><label for="caret-button">Add New Property</label></h3>
+        <div id="caret-button">${caretRightIcon}</div>
+    </div>
     <div id="new-prop-form">
         <div class="row">
+            <label for="new-prop-name">Property Name </label>
             <input type="text" id="new-prop-name" placeholder="property-name"/>
         </div>
         <div class="row">
@@ -48,7 +118,11 @@ const templateString = `
             </select>
         </div>
         <div class="row">
+            <label for="default-value">Default value </label>
             <input type="text" id="default-value" placeholder="Default value" disabled/>
+        </div>
+        <div class="row" id="submit-control">
+            <button id="submit-prop">Create</button>
         </div>
     </div>
 </div>
@@ -75,7 +149,10 @@ class EditorCustomList extends HTMLElement {
 
         // Bound methods
         this.render = this.render.bind(this);
+        this.resetForm = this.resetForm.bind(this);
         this.onDefaultNewTypeChange = this.onDefaultNewTypeChange.bind(this);
+        this.onCaretClick = this.onCaretClick.bind(this);
+        this.onCreateSubmit = this.onCreateSubmit.bind(this);
         //this.onInput = this.onInput.bind(this);
         //this.onFilterClearClick = this.onFilterClearClick.bind(this);
     }
@@ -85,6 +162,13 @@ class EditorCustomList extends HTMLElement {
             this.newPropTypeSelect = this._shadowRoot.getElementById('default-val-select');
             this.newPropNameInput = this._shadowRoot.getElementById('new-prop-name');
             this.newPropDefaultValue = this._shadowRoot.getElementById('default-value');
+            this.addPropControl = this._shadowRoot.getElementById('add-prop-dropdown-control');
+            this.newPropForm = this._shadowRoot.getElementById('new-prop-form');
+            this.createButton = this._shadowRoot.getElementById('submit-prop');
+
+            // Add listeners
+            this.addPropControl.addEventListener('click', this.onCaretClick);
+            this.createButton.addEventListener('click', this.onCreateSubmit);
         }
     }
 
@@ -122,7 +206,43 @@ class EditorCustomList extends HTMLElement {
         } else {
             this.newPropDefaultValue.removeAttribute('disabled');
         }
-        
+    }
+
+    onCaretClick(event){
+        let newPropArea = this._shadowRoot.getElementById('new-prop-area');
+        newPropArea.classList.toggle('open');
+    }
+
+    onCreateSubmit(event){
+        if(this.model){
+            let propName = this.newPropNameInput.value;
+            let defaultValue = this.newPropDefaultValue.value;
+            if(this.newPropDefaultValue.type == 'checkbox'){
+                defaultValue = this.newPropDefaultValue.checked;
+            }
+
+            // Send the property create message
+            this.model.sendMessage({
+                type: 'command',
+                commandName: 'newProperty',
+                args: [
+                    propName,
+                    this.model.id
+                ]
+            }, this.model);
+
+            // Set the created prop to the default
+            // value
+            this.model.partProperties.setPropertyNamed(
+                this.model,
+                propName,
+                defaultValue
+            );
+        }
+    }
+
+    resetForm(){
+        // Nothing for now
     }
 };
 
