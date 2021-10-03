7c1241a4079e04a548f701e068908f88e3a90248
dkrasner <krasnerdaniel@gmail.com>
2021-10-03 12:57:41 +0200

    adding create semantics in EditorMessanger if none
    the underlying part model might not have semantics created. For example
    if no script has been set for it. This causes and error.

--- js/objects/views/editors/EditorMessenger.js
+++ js/objects/views/editors/EditorMessenger.js
@@ -1,4 +1,4 @@
-
+import interpreterSemantics from '../../../ohm/interpreter-semantics.js';
 
 // PREAMBLE
 const templateString = `
@@ -44,7 +44,7 @@ class EditorMessenger extends HTMLElement {
         if(this.isConnected){
             this.sendButton = this._shadowRoot.querySelector('button');
             this.sendButton.addEventListener('click', this.sendMessageFromText);
-        
+
             this.messageField = this._shadowRoot.querySelector('textarea');
             this.messageField.addEventListener('input', this.onMessageFieldInput);
         }
@@ -57,7 +57,7 @@ class EditorMessenger extends HTMLElement {
 
     render(aModel){
         this.model = aModel;
-        
+
         let partTypeLabel = this._shadowRoot.querySelector('h3 > span');
         partTypeLabel.textContent = this.model.type;
     }
@@ -70,6 +70,15 @@ class EditorMessenger extends HTMLElement {
         let text = this.messageField.value + '\n';
         let parsed = window.System.grammar.match(text, 'StatementList');
         if(parsed.succeeded()){
+            // it's possible that no script has been compiled on this part
+            // and hence no semantics exist
+            if(!this.model_semantics){
+                this.model._semantics = window.System.grammar.createSemantics();
+                this.model._semantics.addOperation(
+                    'interpret',
+                    interpreterSemantics(this.model, window.System)
+                );
+            }
             this.model._semantics(parsed).interpret();
         }
     }
