b52e3bf1e880c1bf884967a695c509a32909c4f3
launeh <launeh@gmail.com>
2023-03-05 07:15:16 -0500

    chore: add serve-dev command to package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     browser: true,
     es2021: true,
     mocha: true,
-    commonjs: true
+    commonjs: true,
+    node: true,
   },
   extends: 'eslint:recommended',
   overrides: [
